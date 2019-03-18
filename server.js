ad16c2b214417ec10229c7402182a58d7f9de491
pratibha <pratibhaagarkar@gmail.com>
2019-03-18 14:17:43 +0530

    add project page

--- server.js
+++ server.js
@@ -48,7 +48,11 @@ res.render('about.hbs',{
   pageTitle:'About page'
 });
 });
-
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Project page'
+  });
+});
 app.get('/bad',(req,res)=> {
   res.send({
     errorMessage:'unable to fetch your request'
